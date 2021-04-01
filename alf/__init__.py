a89a92f702ee3a5e6fd9c4234e79e3e5ac636efd
Daniil Larionov <rexhaif.io@gmail.com>
2021-04-01 17:35:58 +0300

    fix imports

--- alf/__init__.py
+++ alf/__init__.py
@@ -1,6 +1,6 @@
 __version__ = "0.1.0"
 
-from distributions import *
-from sklearn_api import AlfSearchCV
-from surrogate import *
-from parameter import *
+from .distributions import *
+from .sklearn_api import AlfSearchCV
+from .surrogate import *
+from .parameter import *
